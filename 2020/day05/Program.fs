abafb4599694e710e00b2c8f7d65954d18b9daa7
Jon Roberts <bigjonroberts@users.noreply.github.com>
2020-12-04 23:58:03 -0600

    day 5, part b solution

--- 2020/day05/Program.fs
+++ 2020/day05/Program.fs
@@ -13,20 +13,39 @@ let selectHalf (minC: char, maxC: char) (minVal, maxVal) c =
 
 [<EntryPoint>]
 let main argv =
-    "input.txt"
-    |> readLines
-    |> Seq.map (fun pass ->
-        let row =
-            pass
-            |> Seq.take 7
-            |> Seq.fold (selectHalf ('F','B')) (0,127)
-            |> fst
-        let column =
-            pass
-            |> Seq.skip 7
-            |> Seq.fold (selectHalf ('L','R')) (0,7)
-            |> fst
-        (row * 8) + column)
+    let seats =
+        "input.txt"
+        |> readLines
+        |> Seq.map (fun pass ->
+            let row =
+                pass
+                |> Seq.take 7
+                |> Seq.fold (selectHalf ('F','B')) (0,127)
+                |> fst
+            let column =
+                pass
+                |> Seq.skip 7
+                |> Seq.fold (selectHalf ('L','R')) (0,7)
+                |> fst
+            {| Row = row; Column = column; SeatID = (row * 8) + column |})
+    let seatIds = 
+        seats
+        |> Seq.map (fun seat -> seat.SeatID)
+    seatIds
     |> Seq.max
     |> printfn "Part 1: %i"
+
+    let allSeatIds =
+        seq { 1 .. 126 }
+        |> Seq.collect (fun row ->
+            seq { 0 .. 7 }
+            |> Seq.map (fun column -> row * 8 + column))
+    allSeatIds
+    |> Seq.filter (fun seatId ->
+        not (Seq.exists ((=) seatId) seatIds))
+    |> Seq.find (fun seatId ->
+        Seq.exists ((=) (seatId+1)) seatIds
+            && Seq.exists ((=) (seatId-1)) seatIds)
+    |> printfn "part 2: %i"
+
     0
