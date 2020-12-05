aaa3f051d3e3afc361372b71ef8d6bde3a0b9ab0
Jon Roberts <jon.roberts@olo.com>
2020-12-04 21:46:17 -0600

    day 1, part b solution as dynamic recursive

--- 2020/day01/Program.fs
+++ 2020/day01/Program.fs
@@ -5,29 +5,32 @@ let readLines (filePath:string) = seq {
     while not sr.EndOfStream do
         yield sr.ReadLine () }
 
-let findTotal (targetTotal:int) xs =
-    xs
-    |> Array.indexed
-    |> Array.choose (fun (index,value) -> 
+let findTotal (targetTotal:int) (numFactors: int) xs =
+    let xs = Array.indexed xs
+    let rec recFindTotal (index: int) (depth: int) (factors: int list) =
+        let calculate (index,value) =
+            if depth = numFactors then
+                let newFactors = value::factors
+                if newFactors |> List.reduce (+)  = targetTotal then
+                    Some newFactors
+                else None
+            else
+                recFindTotal index (depth+1) (value::factors)
         xs
         |> Array.skip index
-        |> Array.indexed
-        |> Array.choose (fun (index1,value1) ->
-            xs
-            |> Array.skip index1
-            |> Array.indexed
-            |> Array.choose (fun (index2,value2) -> if value + value1 + value2  = targetTotal then Some (value, value1, value2) else None)
-            |> Array.tryHead)
-        |> Array.tryHead)
-    |> Array.tryHead
+        |> Array.choose calculate
+        |> Array.tryHead
+    recFindTotal 0 0 List.empty
+
+let calcProduct input i =
+    input
+    |> findTotal 2020 i
+    |> Option.map (List.reduce (*))
+    |> Option.iter (printfn "Answer part %i: %i" i)
 
 [<EntryPoint>]
 let main argv =
 
     let input = "input.txt" |> readLines |> Seq.map int |> Array.ofSeq
-    input
-    |> findTotal 2020
-    |> Option.map (fun (x,y,z) -> x * y * z)
-    |> Option.iter (printfn "Answer: %i")
-
+    { 1 .. 2 } |> Seq.iter (calcProduct input)
     0
