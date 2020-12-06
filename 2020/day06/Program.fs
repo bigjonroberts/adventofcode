846d633253be0ca2d5af5ccd004660e395c16a11
Jon Roberts <bigjonroberts@users.noreply.github.com>
2020-12-06 16:59:48 -0600

    2020 day 6, part 2

--- 2020/day06/Program.fs
+++ 2020/day06/Program.fs
@@ -1,5 +1,5 @@
 
-let rec readGroup (sr: System.IO.StreamReader) (group: string) =
+let rec readGroup (sr: System.IO.StreamReader) (group: string list) =
     if sr.EndOfStream then
         group
     else
@@ -7,20 +7,39 @@ let rec readGroup (sr: System.IO.StreamReader) (group: string) =
         if line = "" then
             group
         else
-            readGroup sr (group + line)
+            readGroup sr (line::group)
 
 let readLines (filePath:string) = seq {
     use sr = new System.IO.StreamReader (filePath)
     while not sr.EndOfStream do
-        yield readGroup sr "" }
+        yield readGroup sr List.empty }
 
 let countChar (charCount: Set<char>) (c: char) =
     Set.add c charCount
 
+module Part2 =
+    let countChar (charCount: Set<char>) (answers: char seq) =
+        answers
+        |> Seq.filter (fun c -> Set.contains c charCount)
+        |> Set.ofSeq
+
 [<EntryPoint>]
 let main argv =
-    readLines "input.txt"
-    |> Seq.map ( Seq.fold countChar Set.empty<char>)
-    |> Seq.sumBy (Set.count)
+    let groups = "input.txt" |> readLines |> Seq.cache
+    groups
+    |> Seq.map (
+        List.toSeq >> (Seq.collect id) // converts to a single line
+        >> Seq.fold countChar Set.empty<char>) // counts
+    |> Seq.sumBy Set.count
     |> printfn "Part 1: %i"
+
+    groups
+    |> Seq.map (fun group ->
+        let firstSet = group |> Seq.head |> Set.ofSeq
+        group
+        |> Seq.tail
+        |> Seq.fold Part2.countChar firstSet)
+    |> Seq.sumBy Set.count
+    |> printfn "Part 2: %i"
+
     0
