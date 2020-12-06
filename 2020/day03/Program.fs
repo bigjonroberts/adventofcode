752565c2bbfff863f5196611b450481839e95c17
Jon Roberts <bigjonroberts@users.noreply.github.com>
2020-12-06 17:53:47 -0600

    day 3, part 2

--- 2020/day03/Program.fs
+++ 2020/day03/Program.fs
@@ -4,21 +4,35 @@ let readLines (filePath:string) = seq {
     while not sr.EndOfStream do
         yield sr.ReadLine () }
 
-let slide (position: int, treeCount: int) (treeMap: char seq)  =
-    let width = Seq.length treeMap
-    let position = if width > 0 then position % width else position
-    let treeCount =
-        if Seq.item position treeMap = '#' then
-            treeCount + 1
-        else
-            treeCount
-    (position + 3, treeCount)
+let slide (xslide: int, yslide: int) (xposition: int, yposition: int, treeCount: int64) (treeMap: char seq)  =
+    if yposition % yslide = 0 then
+        let width = Seq.length treeMap
+        let position = if width > 0 then xposition % width else xposition
+        let treeCount =
+            if Seq.item position treeMap = '#' then
+                treeCount + 1L
+            else
+                treeCount
+        (xposition + xslide, yposition + 1, treeCount)
+    else
+        (xposition, yposition + 1, treeCount)
+
+let calcTrees input slope =
+    input
+    |> Seq.fold (slide slope) (0,0,0L)
+    |> fun (_,_,x) -> x
 
 [<EntryPoint>]
 let main argv =
-    "input.txt"
-    |> readLines
-    |> Seq.fold slide (0,0)
-    |> snd
+    let input = "input.txt" |> readLines |> Seq.cache
+    let calcTrees = calcTrees input
+
+    calcTrees (3,1)
     |> printfn "Part 1: %i"
+
+    [ (1,1); (3,1); (5,1); (7,1); (1,2) ]
+    |> List.map (calcTrees >> fun x -> printfn "%i" x; x)
+    |> List.reduce (*)
+    |> printfn "Part 2: %i"
+
     0
