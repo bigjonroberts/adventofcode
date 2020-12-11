d56d22573b65f6bdadab1b549a5b2d1d2d988b18
Jon Roberts <bigjonroberts@gmail.com>
2020-12-10 23:04:46 -0600

    day 10 init

---
+++ 2020/day10/Program.fs
@@ -0,0 +1,12 @@
+﻿
+let readLines (filePath:string) = seq {
+    use sr = new System.IO.StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine () }
+
+[<EntryPoint>]
+let main argv =
+
+    
+
+    0
